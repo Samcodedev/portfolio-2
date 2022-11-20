e595c8d10a861028064062680c3956c9cc109cd9
Samcodedev <obanlasamuelolakunle@gmail.com>
2022-11-20 13:07:18 +0100

    update

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { VscHome } from 'react-icons/vsc'
+import { BsPersonCircle } from 'react-icons/bs'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { HiOutlineMail } from 'react-icons/hi'
+
+
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <ul>
-        <Link to="/"><li>Home</li></Link>
-        <Link to="/about"><li>About</li></Link>
-        <button>Contact Me</button>
-      </ul>
+      <div className="dd">
+        <GiHamburgerMenu fontSize={45} id="nav" />
+        <ul>
+          <Link to="/">
+            <li id='lli'><VscHome id='icon' /><span id='tut'>Home</span></li>
+          </Link>
+          <Link to="/about">
+            <li id='lli'><BsPersonCircle id='icon' /><span id='tut'>About</span></li>
+          </Link>
+          <button><span id='mails'>Contact Me</span> <HiOutlineMail id='mail' /></button>
+        </ul>
+      </div>
     </div>
   )
 }
