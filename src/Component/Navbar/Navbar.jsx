b22a7ce20b993268c7f1ea8c97ae6cb4a1b38b97
Samcodedev <obanlasamuelolakunle@gmail.com>
2023-02-04 16:19:52 +0100

    navbar update

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
-import { VscHome } from 'react-icons/vsc'
-import { BsPersonCircle } from 'react-icons/bs'
 import { GiHamburgerMenu } from 'react-icons/gi'
-import { HiOutlineMail } from 'react-icons/hi'
+import { GiCancel} from 'react-icons/gi'
+import { useState } from 'react'
+import logo from '../img/Gloria-01.png'
 
 
 
 const Navbar = () => {
+
+  const [open, openFunc] = useState(false)
+  function navbar() {
+    openFunc(!open)
+  }
+
+  let cancel = document.getElementById("cancel")
+
+  setInterval(() => {
+    window.innerWidth >= 970 ? openFunc(true) : console.log("yeeeh")
+    window.innerWidth >= 970 ? cancel.style.display="none" : console.log("hohohohho")
+  }, 100);
+
   return (
     <div className='navbar'>
-      <div className="dd">
-        <GiHamburgerMenu fontSize={45} id="nav" />
+      <img src={logo} alt="" />
+      <GiHamburgerMenu fontSize={35} onClick={navbar} style={{display: open? "none" : "block"}} />
+      <GiCancel id='cancel' fontSize={35} onClick={navbar} style={{display: open? "block": "none"}} />
+      <div className="dd" style={{display: open ? "flex" : "none"}}>
         <ul>
-          <Link to="/">
-            <li id='lli'><VscHome id='icon' /><span id='tut'>Home</span></li>
-          </Link>
-          <Link to="/about">
-            <li id='lli'><BsPersonCircle id='icon' /><span id='tut'>About</span></li>
-          </Link>
-          <button><span id='mails'>Contact Me</span> <HiOutlineMail id='mail' /></button>
+          <Link to="/" onClick={navbar}><li>Home</li></Link>
+          <Link to="/about" onClick={navbar}><li>About</li></Link>
+          <Link to="" onClick={navbar}><li>Contact Me</li></Link>
         </ul>
       </div>
     </div>
